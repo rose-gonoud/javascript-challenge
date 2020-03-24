f2cb2e533ec30568ae0c1e6904477ee017280bc3
Rose <rose.gonoud@gmail.com>
2020-03-24 14:33:45 -0700

    everything working except date filter table edits

--- UFO-level-1/README.md
+++ UFO-level-1/README.md
@@ -38,7 +38,7 @@ You can handle this... right? The planet Earth needs to know what we have found!
 
 * Complete all of Level 1 criteria.
 
-* Using multiple `input` tags and/or select dropdowns, write JavaScript code so the user can to set multiple filters and search for UFO sightings using the following criteria based on the table columns:
+* Using multiple `input` tags and/or select dropdowns, write JavaScript code so the user can set multiple filters and search for UFO sightings using the following criteria based on the table columns:
 
   1. `date/time`
   2. `city`
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,11 +4,54 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
+// Console.log the data from data.js
 console.log(data);
 
-// Step 1: Loop Through `data` and console.log each object
-data.forEach(function(alienReport) {
-    console.log(alienReport);
-  });
+// More verbose solution:
 
+// Step 1: Loop Through `data` and console.log each object
+// data.forEach(function(alienReport) {
+//     console.log(alienReport);
+//     // appending the <tr> elements
+//     var row = tbody.append("tr");
+//     // checking the itertion over each value
+//     Object.entries(alienReport).forEach(function([key, value]) {
+//         console.log(key, value);
+//     // Append a cell to the row for each value in the alien report objects
+//     var cell = row.append("td");
+//     cell.text(value);
+//   });
+// });
+
+// Cool arrow function solution:
+
+data.forEach((alienReport) => {
+    var row = tbody.append("tr");
+    Object.entries(alienReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
+
+
+// reference to the button's ID
+var button = d3.select("#filter-btn");
+// reference to the input element thru its ID
+var inputField = d3.select("#datetime");
+
+// click handler signals click was registered w/ console text
+button.on("click", function() {
+  console.log("new search date incoming!");
+});
+
+// triggering a change event to filter the table when a new date is entered
+// logs the input date for testing purposes
+inputField.on("change", function() {
+  var inputDate = d3.event.target.value;
+  console.log(inputDate);
+
+  var filteredData = data.filter(datum => datum.datetime === inputDate);
+
+
+//   console.log(filteredData);
+});
